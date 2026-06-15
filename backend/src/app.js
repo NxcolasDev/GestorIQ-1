7abf499bef4e9da739b6886d6197805a109c330e
mtmaciel <mtmaciel7@gmail.com>
2026-06-15 14:02:01 -0300

    alinhada rota de produtos e documentacao do swagger

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,6 @@ const swaggerDocument = require('./docs/swagger');
 const productRoutes = require('./routes/productRoutes');
 const app = express();
 
-// Rotas e middlewares adicionais podem ser adicionados aqui pelos desenvolvedores.
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/produtos', productRoutes);
